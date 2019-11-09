685103cbd9eaa88b2d80d8944f0434ade24df242
JimmyVazz <cjvasquez_98@hotmail.com>
2019-11-09 13:33:55 -0600

    iteracion 2

--- interacion1.js
+++ interacion1.js
@@ -2,3 +2,12 @@ let hacker1 = "Jimmy"
 console.log(hacker1)
 let hacker2 = "Krome"
 console.log(hacker2)
+
+if(hacker1.length > hacker2.length){
+    console.log("El conductor" + hacker1 + "es mas largo con " + hacker1.length + " caracteres") 
+}else if(hacker1.length === hacker2.length){
+    console.log("Tienen el mismo tamaño con " + hacker1.length + " caracteres")
+}else{
+    console.log("El navegador" + hacker1 + "es mas largo con " + hacker2.length + " caracteres")
+}
+
